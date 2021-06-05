03acd07b21c2f1f58a1a027d3d30836e79d342c4
Aniket-Dey2000 <84792843+Aniket-Dey2000@users.noreply.github.com>
2021-06-06 05:09:00 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>React Js Bootcamp Completion . Thanksgiving to Shape AI</h1>
+      <p>
+        Hello my name is Aniket Dey i am a student of Techno india University WB
+        and this is my project. I have successfully completed the basic web
+        development and react js bootcamp and have learnt the basics , All
+        thanks to ShapeAI and specially to Shaurya sir for always being there to
+        help solve our doubts and helping us learn .
+      </p>
+    </div>
+  );
+}
+
+export default Note;
